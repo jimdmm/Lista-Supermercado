fd17875abd8954bd27b8d76b20e89c7d10bccc22
Jimdmm <jimdouglasmmferreira@gmail.com>
2022-12-14 06:53:28 -0300

    #create-element

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,4 +10,15 @@ form.addEventListener("submit", (e) => {
 function criaElemento(nome, quantidade) {
     console.log(nome)
     console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
 }
