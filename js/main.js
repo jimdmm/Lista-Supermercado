b305d6941554613df84e386f9214b03a8fd87783
Jimdmm <jimdouglasmmferreira@gmail.com>
2022-12-14 13:08:03 -0300

    #criando_localStorage

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ const lista = document.getElementById("lista")
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    criaElemento(e.target.elements['nome'].value, e.target.elements['quantidade'].value)
+    const nome = e.target.elements['nome']
+    const quantidade = e.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
@@ -21,4 +27,12 @@ function criaElemento(nome, quantidade) {
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+
 }
+
