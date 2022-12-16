a2c22a7ae9b7664d29c302ee9be631820dc0ac2b
jimdmm <jimdouglasmmferreira@gmail.com>
2022-12-15 21:58:10 -0300

    #Finalização_lista

--- js/main.js
+++ js/main.js
@@ -23,8 +23,10 @@ if (existe) {
     itemAtual.id = existe.id
 
     atualizaElemento(itemAtual)
+
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
 } else {
-    itemAtual.id = itens.length
+    itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
     criaElemento(itemAtual)
 
@@ -45,14 +47,38 @@ function criaElemento(item) {
     const numeroProduto = document.createElement('strong')
     numeroProduto.innerHTML = item.quantidade
     numeroProduto.dataset.id = item.id
-
     novoProduto.appendChild(numeroProduto)
+
     novoProduto.innerHTML += item.produto
 
     lista.appendChild(novoProduto)
+
+    novoProduto.appendChild(botaoDeleta(item.id))
 
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    
+    let imgDelete = document.createElement('img');
+    imgDelete.src = 'delete.png';
+    elementoBotao.appendChild(imgDelete)
+
+    
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
